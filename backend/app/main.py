0a0318502a9a994373eee2562c3981bc2a13c7a7
imriyamandal <riyamandal.email@gmail.com>
2026-08-24 02:28:19 +0530

    add the url

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,7 +26,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://your-vercel-app.vercel.app"
+        "https://personal-finance-tracker-ruddy-theta.vercel.app/"
     ], # Allow all origins for dev; specify ports later in production
     allow_credentials=True,
     allow_methods=["*"],
